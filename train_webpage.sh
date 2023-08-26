5db5a3c21032b5729d7d4d1349d8ab0abb0a6129
Shuzheng Si <sishuzheng@foxmail.com>
2023-08-27 01:45:08 +0800

    Update train_webpage.sh

--- train_webpage.sh
+++ train_webpage.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=7 python main.py -mode train -dd dataset/webpage -pm bert -cd save/webpage -rd ../STANT/resource -knn True -k 16 -beta 0.2 -sp 0.7 -hd 128 -bs 12
+CUDA_VISIBLE_DEVICES=0 python main.py -mode train -dd dataset/webpage -pm bert -cd save/webpage -rd resource -knn True -k 16 -beta 0.2 -sp 0.7 -hd 128 -bs 12
